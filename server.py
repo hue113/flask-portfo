3811584e1b8cc7c5c56b8d0307087fa1a582d832
Hue Pham <huepham113@gmail.com>
2021-03-23 15:25:25 -0400

    flask portfolio website

---
+++ server.py
@@ -0,0 +1,53 @@
+from flask import Flask, render_template, request, url_for, redirect
+import csv
+app = Flask(__name__)
+print(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('./index.html')
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
+# @app.route('/index.html')
+# def home():
+#     return render_template('./index.html')
+
+
+def write_to_file(data):
+    with open('database.txt', mode='a') as database:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        file = database.write(f'\n{email},{subject},{message}')
+        
+        
+# https://docs.python.org/3/library/csv.html
+def write_to_csv(data):
+    with open('database.csv', newline='', mode='a') as database2:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        csv_writer = csv.writer(database2, delimiter=',',
+                                quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email,subject,message])
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        try:
+            data = request.form.to_dict() # turn data into dict
+            # write_to_file(data)
+            write_to_csv(data)
+            return redirect('/thankyou.html') # dynamic route --> ko can ./templates ...
+        except:
+            return 'did not save to database'
+    else:
+        return 'Something went wrong, try again.'
+
+
+# @app.route('/<username>/<int:post_id>')
+# def hello_world(username=None, post_id=None):
+#     return render_template('./index.html', name=username, post_id=post_id)
